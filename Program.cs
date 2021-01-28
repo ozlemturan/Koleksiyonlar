2e554b7db50c746ef18cbe841fa80bb94aa3da14
ozlemturan <65733862+ozlemturan@users.noreply.github.com>
2021-01-28 15:33:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Koleksiyonlar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //string[] isimler = new string[] { "Ozlem", "Gokhan" };
+            //Console.WriteLine(isimler[0]);
+            //Console.WriteLine(isimler[1]);
+
+
+            List<string> isimler2 = new List<string>() {"Ozlem","Gokhan" };
+            Console.WriteLine(isimler2[0]);
+            Console.WriteLine(isimler2[1]);
+
+            isimler2.Add("Atik");
+            Console.WriteLine(isimler2[2]);
+            Console.WriteLine(isimler2[0]);
+        }
+    }
+}
